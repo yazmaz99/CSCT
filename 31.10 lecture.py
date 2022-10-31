35c421998bc2741fbb5115a7da91d24a38b8d847
Yas <117076418+yazmaz99@users.noreply.github.com>
2022-10-31 10:51:13 +0000

    Update 31.10 lecture.py
    Call function drawSide 4 times to draw square

--- 31.10 lecture.py
+++ 31.10 lecture.py
@@ -5,6 +5,11 @@ userLength = int(input("Enter length of sides: "))
 def drawSide(userLength):
     turtle.forward(userLength)
     turtle.right(90)
-    return
+    return 
+
+drawSide(userLength)
+drawSide(userLength)
+drawSide(userLength)
+drawSide(userLength)
 
 input()
