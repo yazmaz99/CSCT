ae5addf51631083556d7548e4bf46fae6f630b55
Yas <117076418+yazmaz99@users.noreply.github.com>
2022-10-31 10:39:04 +0000

    Update 31.10 lecture.py

--- 31.10 lecture.py
+++ 31.10 lecture.py
@@ -2,12 +2,9 @@ import turtle
 
 userLength = int(input("Enter length of sides: "))
 
-turtle.forward(userLength)
-turtle.right(90)
-turtle.forward(userLength)
-turtle.right(90)
-turtle.forward(userLength)
-turtle.right(90)
-turtle.forward(userLength)
-turtle.right(90)
+def drawSide(userLength):
+    turtle.forward(userLength)
+    turtle.right(90)
+    return
+
 input()
